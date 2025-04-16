f5c73aa4df41cc09514b8bc543b81fd829ab8925
theoX443 <203435033+theoX44353@users.noreply.github.com>
2025-04-17 00:15:15 +0700

    Create Constants.kt

---
+++ src/main/java/com/google/devsite/renderer/converters/impl/paths/Constants.kt
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.devsite.renderer.impl.paths
+
+internal const val DIR_INDEX_NAME = "index"
+internal const val PACKAGE_SUMMARY_NAME = "package-summary"
+
+internal const val MACHINE_PACKAGE_LIST_FILE = "package-list"
+internal const val DIR_INDEX_FILE = "$DIR_INDEX_NAME.html"
+internal const val PACKAGE_INDEX_FILE = "packages.html"
+internal const val CLASS_INDEX_FILE = "classes.html"
+internal const val TOC_FILE = "_toc.yaml"
+internal const val BOOK_FILE = "_book.yaml"
+internal const val PACKAGE_SUMMARY_FILE = "$PACKAGE_SUMMARY_NAME.html"
