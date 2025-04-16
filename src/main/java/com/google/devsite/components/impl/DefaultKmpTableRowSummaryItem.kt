c137a697b1cad5aeee2a76887b00f53b9b356166
theoX443 <203435033+theoX44353@users.noreply.github.com>
2025-04-16 23:39:20 +0700

    Create DefaultKmpTableRowSummaryItem.kt

---
+++ src/main/java/com/google/devsite/components/impl/DefaultKmpTableRowSummaryItem.kt
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.devsite.components.impl
+
+import com.google.devsite.components.ContextFreeComponent
+import com.google.devsite.components.table.KmpTableRowSummaryItem
+import kotlinx.html.TR
+import kotlinx.html.code
+import kotlinx.html.td
+
+/** Default implementation of the two-pane layout item for symbol tables. */
+internal data class DefaultKmpTableRowSummaryItem<
+    T : ContextFreeComponent?,
+    V : ContextFreeComponent,
+>(
+    override val data: KmpTableRowSummaryItem.Params<T, V>,
+) : KmpTableRowSummaryItem<T, V> {
+    override fun render(into: TR) =
+        into.run {
+            data.title?.let { title -> td { code { title.render(this) } } }
+            td { data.description.render(this) }
+            td { data.platforms.render(this) }
+        }
+
+    override fun toString() = (data.title?.let { "$it: " } ?: "") + data.description.toString()
+}
