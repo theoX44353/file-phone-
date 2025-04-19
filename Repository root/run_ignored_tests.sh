8c5dedaee582bdd411d16a2b5237de5af1141496
theoX443 <203435033+theoX44353@users.noreply.github.com>
2025-04-20 02:32:13 +0700

    Delete Repository root directory

--- Repository root/run_ignored_tests.sh
+++
@@ -1,3 +0,0 @@
-find ./src/test/java/com/google/devsite/ -name "*.kt" -exec sed -i '' 's/@Ignore/\/\/ @DontIgnore/g' {} +
-./gradlew test
-find ./src/test/java/com/google/devsite/ -name "*.kt" -exec sed -i '' 's/\/\/ @DontIgnore/@Ignore/g' {} +
